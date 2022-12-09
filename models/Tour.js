9a8c339ad95185fec061e68a52ba4d05a65ed8c3
karimibrahimramadan <karimibrahimramadan@gmail.com>
2022-12-09 10:41:23 +0200

    Api features, and tour model update

--- models/Tour.js
+++ models/Tour.js
@@ -2,10 +2,39 @@ const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    rating: { type: Number, default: 0 },
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "A tour must have a description"],
+      trim: true,
+    },
+    duration: { type: Number, required: [true, "A tour must have a duration"] },
+    summary: { type: String, trim: true },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a max group size"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+      enum: ["Easy", "Medium", "Hard"],
+    },
+    price: { type: Number, required: [true, "A tour must have a price"] },
+    priceDiscount: Number,
+    ratingAverage: { type: Number, default: 0 },
+    ratingQuantity: { type: Number, default: 0 },
+    guides: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"],
+    },
+    images: [String],
+    startDates: [Date],
   },
   { timestamps: true }
 );
---
+++ utils/apiFeatures.js
@@ -0,0 +1,55 @@
+class APIFeatues {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  filter() {
+    const queryStrObj = { ...this.queryStr };
+    const excludedFields = ["sort", "page", "limit", "fields", "keyword"];
+    excludedFields.forEach((element) => {
+      delete queryStrObj[element];
+    });
+    let queryStr = JSON.stringify(queryStrObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse());
+    return this;
+  }
+
+  sort() {
+    this.query = this.queryStr.sort
+      ? this.query.sort(this.queryStr.sort.split(",").join(" "))
+      : this.query.sort("-createdAt");
+    return this;
+  }
+
+  limitFields() {
+    this.query = this.queryStr.fields
+      ? this.query.select(this.queryStr.fields.split(",").join(" "))
+      : this.query.select("-__v");
+    return this;
+  }
+
+  paginate() {
+    const currentPage = this.queryStr.page * 1 || 1;
+    const limit = this.queryStr.limit * 1 || 24;
+    const skip = (currentPage - 1) * limit;
+    this.query = thiq.query.limit(limit).skip(skip);
+    return this;
+  }
+}
+
+module.exports = APIFeatues;
