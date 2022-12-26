d740888297c0e4fc68ee69b930af2302582afb74
karimibrahimramadan <karimibrahimramadan@gmail.com>
2022-12-26 21:39:56 +0200

    security updates

--- app.js
+++ app.js
@@ -5,8 +5,20 @@ require("colors");
 const helmet = require("helmet");
 const routesController = require("./routes/routesController");
 const errorHandler = require("./controllers/errorController");
+const rateLimiter = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const app = express();
+
+const limiter = rateLimiter({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: "Too many requests from this IP, Try again in an hour!",
+});
 
 // middlewares
 app.use(helmet());
@@ -15,6 +27,10 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use("/api/v1", limiter);
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
 
 // routes
 app.use("/api/v1/tours", routesController.tourRouter);
